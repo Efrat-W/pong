d1fd2246a0aa1433220d686fe17873e24d8297a7
ef-w <efratart2161@gmail.com>
2023-11-26 17:34:30 +0200

    Collision control

--- entity.py
+++ entity.py
@@ -11,7 +11,7 @@ class Paddle:
         #pos
         self.y = game.H // 2 - self.length // 2
         self.vel = 0.5
-        
+        self.score = 0
 
     def update(self, pos=0):
         self.y += pos*self.vel
@@ -27,6 +27,12 @@ class Ball:
         self.pos = [game.W // 2, game.H // 2]
         self.vel = 0.4
         self.dir = [1,1]
+
+    def collision(self, paddles):
+        bounding_box = [self.pos[0] - self.radius, self.pos[1] - self.radius, self.radius * 2, self.radius * 2]
+        for paddle in paddles:
+            if pygame.Rect(paddle.x, paddle.y, paddle.width, paddle.length).colliderect(bounding_box):
+                return True
 
     def update(self):
         self.pos[0] += self.dir[0] * self.vel
